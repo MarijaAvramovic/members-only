7d741fb19296bc4edf386612d4c72499e0a83dac
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-04-02 15:09:06 -0400

    create db and pool conection to local db

---
+++ db/pool.js
@@ -0,0 +1,15 @@
+const { Pool } = require("pg");
+
+ 
+module.exports = new Pool({
+  host: "localhost",
+  user: "marijaavramovic",
+  database: "members_only",
+  password: "",
+  port: 4441
+});
+
+ 
+// module.exports = new Pool({
+//   connectionString: "postgresql://marijaavramovic:@localhost:4441/members_only"
+// });
